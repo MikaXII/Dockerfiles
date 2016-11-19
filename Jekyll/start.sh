6e702db2ce2fd4ee1402092a26b37f5a6f514613
MikaXII <mikael.joubert@supinfo.com>
2016-11-19 10:54:22 +0100

    Update start.sh

--- Jekyll/start.sh
+++ Jekyll/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ ! -f stamp_installed ];then
+if [ ! -f .stamp_installed ];then
   git clone $REPO_GIT /usr/src/app
   echo "unset GIT_DIR && cd /usr/src/app && git pull" > .git/hooks/post-update
   bundle install
-  touch stamp_installed
+  touch .stamp_installed
 fi
 
 jekyll s --watch --host 0.0.0.0
