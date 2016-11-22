83cc9f83ee84e08abae7a145694e4be29256f966
MikaXII <mikael.joubert@outlook.com>
2016-11-22 23:03:06 +0100

    add travis build

--- Jekyll/start.sh
+++ Jekyll/start.sh
@@ -2,9 +2,9 @@
 
 if [ ! -f stamp_installed ];then
   git clone $REPO_GIT /usr/src/app
-  echo "unset GIT_DIR && cd /usr/src/app && git pull" > .git/hooks/post-update
+#  echo "unset GIT_DIR && cd /usr/src/app && git pull" > .git/hooks/post-update
   bundle install
   touch stamp_installed
 fi
-
+travis encrypt GITHUB_TOKEN=$GITHUB_TOKEN --add
 jekyll s --watch --host 0.0.0.0
